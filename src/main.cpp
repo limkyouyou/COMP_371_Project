f186c10defa57d84555c61ee3c9d2d901979533c
limkyouyou <limkyouyou@gmail.com>
2025-07-24 13:04:15 -0400

    Remove build log from tracking and update .gitignore and resolve merge conflict

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,4 @@
 
-        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) // move camera up
-        {
-            cameraPosition.y -= currentCameraSpeed * dt;
-        }
-
-        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) // move camera down
-        {
-            cameraPosition.y += currentCameraSpeed * dt;
-        }
-
-        // TODO 6
-        // Set the view matrix for first and third person cameras
-        // - In first person, camera lookat is set like below
-        // - In third person, camera position is on a sphere looking towards center
-        mat4 viewMatrix = lookAt(cameraPosition, cameraPosition + cameraLookAt, cameraUp);
-
-        GLuint viewMatrixLocation = glGetUniformLocation(shaderProgram, "viewMatrix");
-        glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, &viewMatrix[0][0]);
-
-
-        // @TODO 2 - Shoot Projectiles
-        //
-        // shoot projectiles on mouse left click
-        // To detect onPress events, we need to check the last state and the current state to detect the state change
-        // Otherwise, you would shoot many projectiles on each mouse press
-        // ...
-
-
-    }
-
-
-    // Shutdown GLFW
-    glfwTerminate();
-
-    return 0;
-}
-=======
 #include <app.h>
 #include <iostream>
 
@@ -46,4 +9,3 @@ int main() {
 
 	return app.run();
 }
->>>>>>> kyo
